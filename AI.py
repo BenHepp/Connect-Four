ca55994d9b57e95da9e3c32dc96ddf65a456b4ff
Benjamin Hepp <benhepp1414@gmail.com>
2021-05-23 21:21:34 -0400

    added tie-breaker

--- AI.py
+++ AI.py
@@ -9,40 +9,45 @@ import csv
 headers = None
 
 class AI:
-
+    
     def __init__(self, debut):
         self.prev_moves = {}
         self.game_moves = []
         self.debut = debut
         self.read_in_prev_moves()
-
+    
     def next_move(self, board_chips, moves_so_far, gameboard, color):
-
+        
         col = None
         col_scores = np.zeros(7, dtype=float)
-
+        
         for column in range(7):
             col_scores[column] = self.evaluate_column(board_chips, moves_so_far, column, gameboard, color)
-        col = np.argmax(col_scores)
+        
+        col = self.break_ties_randomly(col_scores)
+        self.game_moves.append(moves_so_far + str(col))
+        
+    return col
 
-        max_val = col_scores[col]
-        index_arr = []
+def break_ties_randomly(self, col_scores):
+    col = np.argmax(col_scores)
+    max_val = col_scores[col]
+    
+    index_arr = []
         index = 0
-        for elt in col_scores:
-            if max_val == elt:
+        for val in col_scores:
+            if max_val == val:
                 index_arr.append(index)
             index += 1
 
-        random_index = random.randint(0,len(index_arr)-1)
-
+    random_index = random.randint(0,len(index_arr)-1)
         col = index_arr[random_index]
-
-        self.game_moves.append(moves_so_far + str(col))
         print('----')
+        
         return col
 
-    def read_in_prev_moves(self):
-        filename = "prev_moves1.csv" if self.debut == "first" else "prev_moves2.csv"
+def read_in_prev_moves(self):
+    filename = "prev_moves1.csv" if self.debut == "first" else "prev_moves2.csv"
         with open(filename, newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             for row in reader:
@@ -54,26 +59,26 @@ class AI:
                 self.prev_moves[move]["score"] = float(score)
                 self.prev_moves[move]["wins"] = int(wins)
                 self.prev_moves[move]["total"] = int(total)
-
-    def write_back_prev_moves(self):
-        filename = "prev_moves1.csv" if self.debut == "first" else "prev_moves2.csv"
-        with open(filename, 'w') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            for move in self.prev_moves:
-                score = self.prev_moves[move]["score"]
-                wins = self.prev_moves[move]["wins"]
-                total = self.prev_moves[move]["total"]
-                row = [move,score,wins,total]
-                csvwriter.writerow(row)
+                    
+                    def write_back_prev_moves(self):
+                        filename = "prev_moves1.csv" if self.debut == "first" else "prev_moves2.csv"
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for move in self.prev_moves:
+        score = self.prev_moves[move]["score"]
+        wins = self.prev_moves[move]["wins"]
+        total = self.prev_moves[move]["total"]
+        row = [move,score,wins,total]
+        csvwriter.writerow(row)
 
     def end_game(self, outcome):
         self.update_prev_move_scores(outcome)
-        #self.write_back_prev_moves()
+    #self.write_back_prev_moves()
 
-    def update_prev_move_scores(self, outcome):
-
-        update = 1 if outcome == "won" else 0
-
+def update_prev_move_scores(self, outcome):
+    
+    update = 1 if outcome == "won" else 0
+        
         for move in self.game_moves:
             if move in self.prev_moves:
                 self.prev_moves[move]["wins"] += update
@@ -81,26 +86,26 @@ class AI:
                 self.prev_moves[move]["score"] = float(self.prev_moves[move]["wins"]) / self.prev_moves[move]["total"]
             else:
                 self.prev_moves[move] = {"score": 1, "wins": 1, "total": 1}
-        self.game_moves = []
+    self.game_moves = []
 
-    def evaluate_column(self, board_chips, moves_so_far, column, gameboard, color):
-
-        col_score = 1
+def evaluate_column(self, board_chips, moves_so_far, column, gameboard, color):
+    
+    col_score = 1
         col = column
         row = board_chips[col]
-
+        
         print("col " + str(col), end=": ")
-
+        
         if board_chips[column] >= 6:
             print("-1")
             return -1
-
+        
         col_score += gameboard[(col, row)].heuristics(gameboard, color, col, row)
         print(str(col_score))
         potential_move = moves_so_far + str(col)
         if potential_move in self.prev_moves:
             col_score *= self.prev_moves[potential_move]["score"]
-
+        
         return col_score
 
 
@@ -110,3 +115,4 @@ class AI:
 
 
 #
+
